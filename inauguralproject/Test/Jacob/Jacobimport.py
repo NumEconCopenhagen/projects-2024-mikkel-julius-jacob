b0bd06c7f48c98e58fb612ba28071f3bf9dc30b2
JacobFreitag <fht348@alumni.ku.dk>
2024-06-18 11:46:41 +0200

    jj

--- inauguralproject/Test/Jacob/Jacobimport.py
+++ inauguralproject/Test/Jacob/Jacobimport.py
@@ -1,73 +1,121 @@
-from types import SimpleNamespace
-
 import numpy as np
 
+class Questien2:
+    # Parameters
+    alpha = 1/3
+    beta = 2/3
+
+    # Utility functions
+    @staticmethod
+    def utility_A(x1, x2):
+        return x1**Questien2.alpha * x2**(1 - Questien2.alpha)
+
+    @staticmethod
+    def utility_B(x1, x2):
+        return x1**Questien2.beta * x2**(1 - Questien2.beta)
+
+    # Demand functions
+    @staticmethod
+    def demand_A(p, omega):
+        p1, p2 = p
+        x1_star = Questien2.alpha * (p1 * omega[0] + p2 * omega[1]) / p1
+        x2_star = (1 - Questien2.alpha) * (p1 * omega[0] + p2 * omega[1]) / p2
+        return np.array([x1_star, x2_star])
+
+    @staticmethod
+    def demand_B(p, omega):
+        p1, p2 = p
+        x1_star = Questien2.beta * (p1 * omega[0] + p2 * omega[1]) / p1
+        x2_star = (1 - Questien2.beta) * (p1 * omega[0] + p2 * omega[1]) / p2
+        return np.array([x1_star, x2_star]) 
+
+    # Market clearing errors
+    @staticmethod
+    def market_clearing_error(p, omega_A, omega_B):
+        demand_A1 = Questien2.demand_A(p, omega_A)
+        demand_B1 = Questien2.demand_B(p, omega_B)
+        error1 = demand_A1[0] + demand_B1[0] - (omega_A[0] + omega_B[0])
+        error2 = demand_A1[1] + demand_B1[1] - (omega_A[1] + omega_B[1])
+        return np.array([error1, error2])
+
+class Question3:
+    # Constants
+    alpha = 1/3
+    beta = 2/3
+    omega_A1 = 0.8
+    omega_A2 = 0.3
+    omega_B1 = 1 - omega_A1
+    omega_B2 = 1 - omega_A2
+    p2 = 1  # Numeraire
+
+    @staticmethod
+    def excess_demand_x1(p1):
+        alpha = Question3.alpha
+        beta = Question3.beta
+        omega_A1 = Question3.omega_A1
+        omega_A2 = Question3.omega_A2
+        omega_B1 = Question3.omega_B1
+        omega_B2 = Question3.omega_B2
+        p2 = Question3.p2
+        
+        xA_star_1 = alpha * (p1 * omega_A1 + p2 * omega_A2) / p1
+        xB_star_1 = beta * (p1 * omega_B1 + p2 * omega_B2) / p1
+        return xA_star_1 + xB_star_1 - (omega_A1 + omega_B1)
+
+    @staticmethod
+    def excess_demand_x2(p1):
+        alpha = Question3.alpha
+        beta = Question3.beta
+        omega_A1 = Question3.omega_A1
+        omega_A2 = Question3.omega_A2
+        omega_B1 = Question3.omega_B1
+        omega_B2 = Question3.omega_B2
+        p2 = Question3.p2
+        
+        xA_star_2 = (1 - alpha) * (p1 * omega_A1 + p2 * omega_A2) / p2
+        xB_star_2 = (1 - beta) * (p1 * omega_B1 + p2 * omega_B2) / p2
+        return xA_star_2 + xB_star_2 - (omega_A2 + omega_B2)
+
+    @staticmethod
+    def total_excess_demand(p1):
+        ed1 = Question3.excess_demand_x1(p1)
+        ed2 = Question3.excess_demand_x2(p1)
+        return abs(ed1) + abs(ed2)
+
 class ExchangeEconomyClass:
-    def __init__(self):
-
-        par = self.par = SimpleNamespace()
-
-        # a. preferences
-        par.alpha = 1/3
-        par.beta = 2/3
-
-        # b. endowments
-        par.w1A = 0.8
-        par.w2A = 0.3
-
-    def utility_A(self,x1A,x2A):
-        pass
-
-    def utility_B(self,x1B,x2B):
-        pass
-
-    def demand_A(self,p1):
-        pass
-
-    def demand_B(self,p1):
-        pass
-
-    def check_market_clearing(self,p1):
-
-        par = self.par
-
-        x1A,x2A = self.demand_A(p1)
-        x1B,x2B = self.demand_B(p1)
-
-        eps1 = x1A-par.w1A + x1B-(1-par.w1A)
-        eps2 = x2A-par.w2A + x2B-(1-par.w2A)
-
-        return eps1,eps2    
-
-    def __init__(self):
-        # Initialize model parameters
-        par = self.par = SimpleNamespace()
-        par.alpha = 1/3  # Preference parameter for consumer A
-        par.beta = 2/3   # Preference parameter for consumer B
-        par.w1A = 0.8    # Initial endowment of good 1 for consumer A
-        par.w2A = 0.3    # Initial endowment of good 2 for consumer A
-        # Calculate initial utility levels based on the endowments
-        self.uA_initial = self.utility_A(par.w1A, par.w2A)
-        self.uB_initial = self.utility_B(1 - par.w1A, 1 - par.w2A)
+    def __init__(self, alpha, beta, w1A, w2A):
+        self.alpha = alpha
+        self.beta = beta
+        self.w1A = w1A
+        self.w2A = w2A
+        self.w1B = 1 - w1A
+        self.w2B = 1 - w2A
 
     def utility_A(self, x1A, x2A):
-        return x1A ** self.par.alpha * x2A ** (1 - self.par.alpha)
+        return x1A**self.alpha * x2A**(1 - self.alpha)
 
-    def utility_B(self, x1B, x2B):
-        return x1B ** self.par.beta * x2B ** (1 - self.par.beta)
+    def demand_A(self, p1):
+        income_A = p1 * self.w1A + self.w2A  # Assuming p2 (price of good 2) is normalized to 1
+        return self.alpha * income_A / p1, (1 - self.alpha) * income_A
 
-    def find_pareto_improvements(self):
-        N = 75
-        x1A_grid = np.linspace(0, 1, N)
-        x2A_grid = np.linspace(0, 1, N)
-        pareto_set = np.zeros((N, N))
-        for i, x1A in enumerate(x1A_grid):
-            for j, x2A in enumerate(x2A_grid):
-                x1B, x2B = 1 - x1A, 1 - x2A
-                uA = self.utility_A(x1A, x2A)
-                uB = self.utility_B(x1B, x2B)
-                if uA >= self.uA_initial and uB >= self.uB_initial:
-                    pareto_set[i, j] = 1
-        return x1A_grid, x2A_grid, pareto_set
+    def demand_B(self, p1):
+        income_B = p1 * self.w1B + self.w2B  # Assuming p2 (price of good 2) is normalized to 1
+        return self.beta * income_B / p1, (1 - self.beta) * income_B
 
-    
+    def find_optimal_allocation(economy, P1):
+        max_utility = -np.inf
+        optimal_price = None
+        optimal_allocation = None
+
+        for p1 in P1:
+            demandB = economy.demand_B(p1)
+            x1A = 1 - demandB[0]
+            x2A = 1 - demandB[1]
+            utility = economy.utility_A(x1A, x2A)
+
+            if utility > max_utility:
+                max_utility = utility
+                optimal_price = p1
+                optimal_allocation = (x1A, x2A)
+
+        return optimal_price, optimal_allocation
